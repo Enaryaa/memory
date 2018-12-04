1ff026988192c7db6b999f3e9b4ab037c021038c
Anne-Sophie Besnard <anne-sophie0.besnard@etu.u-pec.fr>
2018-12-04 13:15:03 +0000

    maj 4/12

--- C/fenetre.c
+++ C/fenetre.c
@@ -6,21 +6,6 @@
 
 int goon = 0;
 
-void menu(){
-
-InitialiserGraphique();   /*créer la fenetre */
-CreerFenetre(200,190,1280,720);
-ChargerImageFond("../Image/projet.png"); 
-
-
-DessinerRectangle(770,350,63,63);
-DessinerRectangle(620,445,63,63);
-DessinerRectangle(810,555,63,63);
-/* ces trois rectangles initialises les boutons de difficultés*/
-DessinerRectangle(159,294,245,242);
-/* ce rectangle initialise le bouton avec les regles du jeu*/
-}
-
 void regle(void){
 	InitialiserGraphique();
 	CreerFenetre(200, 300, 900, 700);
@@ -28,10 +13,43 @@ void regle(void){
 	goon = 1;
 }
 
-int main(void){ 
+void facile(int cartex, int cartey){
+	FermerGraphique();
+	InitialiserGraphique(); /*  grille facile*/
+	CreerFenetre(200, 300, 1280, 720);
+	ChargerImageFond("../Image/fond.png");
+	goon = 1;
+}
 
-	menu();
-	SourisPosition();
+void normal(int cartex, int cartey){
+	FermerGraphique();
+	InitialiserGraphique(); /*grille normal */
+	CreerFenetre(200, 300, 1280, 720);
+	ChargerImageFond("../Image/fond.png");
+	goon = 1;
+}
+
+void difficile(int cartex, int cartey){
+	FermerGraphique();
+	InitialiserGraphique();
+	CreerFenetre(200, 300, 1280, 720); /*grille difficile*/
+	ChargerImageFond("../Image/fond.png");
+	goon = 1;
+}
+
+void menu(){
+
+InitialiserGraphique();   /*créer la fenetre */
+CreerFenetre(200,190,1280,720);
+ChargerImageFond("../Image/projet.png"); 
+
+DessinerRectangle(770,350,63,63);
+DessinerRectangle(620,445,63,63);
+DessinerRectangle(810,555,63,63);
+/* ces trois rectangles initialises les boutons de difficultés*/
+DessinerRectangle(159,294,245,242);
+/* ce rectangle initialise le bouton avec les regles du jeu*/
+SourisPosition();
 	/*permet de connaitre la position actuelle du curseur */
 
 while (goon == 0){
@@ -41,35 +59,24 @@ while (goon == 0){
 		(qui correspond a un bouton) alors on ferme la fenetre actuelle pour en ouvrir une autre contenant les différentes grilles de jeu*/
 		{
 			FermerGraphique();
-			InitialiserGraphique(); /* ouvre grille facile*/
-			CreerFenetre(200, 300, 1280, 720);
-			ChargerImageFond("../Image/fond.png");
-			goon = 1;
+			facile(4, 4);
 		}
 		else if ( _X >= 620 && _X <= 683 && _Y >= 445 && _Y <= 508 )
 		{
 			FermerGraphique();
-			InitialiserGraphique(); /*grille normal */
-			CreerFenetre(200, 300, 1280, 720);
-			ChargerImageFond("../Image/fond.png");
-			goon = 1;
+			normal(7, 5);
 		}
 		else if ( _X >= 810 && _X <= 873 && _Y >= 555 && _Y <= 618 )
 		{
 			FermerGraphique();
-			InitialiserGraphique();
-			CreerFenetre(200, 300, 1280, 720); /*grille difficile*/
-			ChargerImageFond("../Image/fond.png");
-			goon = 1;
+			difficile(8, 6);
 		}
 		else if ( _X >= 159 && _X <= 404 && _Y >= 294 && _Y <= 586 )
 		{
 			FermerGraphique();
 			regle();
 			goon = 1;
-			
-	
-	/*while (booleen2 == 0){
+		/*while (booleen2 == 0){
 		SourisCliquee();
 				FermerGraphique();
 				menu();
@@ -77,8 +84,14 @@ while (goon == 0){
 		}
 	
 	}
+}
 
 
+
+int main(void){ 
+
+	menu();
+	
 
 Touche();
 FermerGraphique();
--- C/jeu.c
+++ C/jeu.c
@@ -3,6 +3,9 @@
 #include<stdlib.h>
 #include<graph.h>
 
+const longueurImage 60;
+const largeurImage 50;
+
 struct card
 {
   char sprite[20];
@@ -10,39 +13,13 @@ struct card
   int y;
   int isReturned;
 };
+int main(int argc, char * argv[]){ 
 
 /*ces valeurs ici on été ajouté pour l'exemple , il fautdra placer les 24 sprites*/
 char sprite[4][50] = { "coeur", "pique", "carreau", "trefle" };
 
 /* liste aleatoire ICI il faut que ce tableau soit généré aleatoirement avec chaque carte en deux exemplaire*/
 char randomize[8][50] = {"coeur", "pique", "coeur", "trefle", "carreau", "trefle", "carreau", "pique"}
-
-int booleen = 0;
-
-void menu(){
-
-InitialiserGraphique();   /*créer la fenetre */
-CreerFenetre(200,190,1280,720);
-ChargerImageFond("../Image/projet.png"); 
-
-
-DessinerRectangle(770,350,63,63);
-DessinerRectangle(620,450,63,63);
-DessinerRectangle(810,560,63,63);
-/* ces trois rectangles initialises les boutons de difficultés*/
-DessinerRectangle(159,294,245,242);
-/* ce rectangle initialise le bouton avec les regles du jeu*/
-}
-
-void regle(){
-  InitialiserGraphique();
-  CreerFenetre(200, 300, 900, 700);
-  ChargerImageFond("../Image/regle22.png");
-  booleen = 1;
-}
-
-int main(int argc, char * argv[]){ 
-
 
   /* rappel : ne pas oublier de définir la constante des longueurs d'image*/
   /*struct card heart = {"heart.png", 10, 20, 0};*/
@@ -53,10 +30,15 @@ int main(int argc, char * argv[]){
   /*de même pour y*/
   int y = marge;
 
+  int ligne =  cartex;
+  int colonne =  cartey;
+
   /* ce tableau doit être rempli grace au tableau randomize pour chaque carte
   struct card tableau[8][6];
   /*on défini une cle pour pour recuperer le sprite de la liste aleatoire,
    il sera incrmenté dans la seoncde boucle for*/
+ 
+
   int random = 0;
   for (int i = 0; i < 8; ++i)
   {
