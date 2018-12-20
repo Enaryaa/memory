fb1fa4752341e76f7feb4ea12147f6d096e64d07
Anne-Sophie Besnard <anne-sophie0.besnard@etu.u-pec.fr>
2018-12-20 18:09:10 +0000

    FINI

--- Programme/grille.c
+++ Programme/grille.c
@@ -28,113 +28,115 @@ void Grille(int cartex, int cartey)
   /*(longueurImage*cartey + marge*cartey + 20),(largeurImage*cartex + marge*cartex +55));*/
   ChargerImageFond("../Image/jeu2.png");
 
-    int sprite[20] = {  
-      ChargerSprite("../Image/carte1.png"),
-      ChargerSprite("../Image/carte2.png"),
-      ChargerSprite("../Image/carte3.png"),
-      ChargerSprite("../Image/carte4.png"),
-      ChargerSprite("../Image/carte5.png"),
-      ChargerSprite("../Image/carte6.png"),
-      ChargerSprite("../Image/carte7.png"),
-      ChargerSprite("../Image/carte8.png"),
-      ChargerSprite("../Image/carte9.png"),
-      ChargerSprite("../Image/carte10.png"),
-      ChargerSprite("../Image/carte11.png"),
-      ChargerSprite("../Image/carte12.png"),
-      ChargerSprite("../Image/carte13.png"),
-      ChargerSprite("../Image/carte14.png"),
-      ChargerSprite("../Image/carte15.png"),
-      ChargerSprite("../Image/carte16.png"),
-      ChargerSprite("../Image/carte17.png"),
-      ChargerSprite("../Image/carte18.png"),
-      ChargerSprite("../Image/carte19.png"),
-      ChargerSprite("../Image/carte20.png")
-    };
+  int sprite[20] = {  
+    ChargerSprite("../Image/carte1.png"),
+    ChargerSprite("../Image/carte2.png"),
+    ChargerSprite("../Image/carte3.png"),
+    ChargerSprite("../Image/carte4.png"),
+    ChargerSprite("../Image/carte5.png"),
+    ChargerSprite("../Image/carte6.png"),
+    ChargerSprite("../Image/carte7.png"),
+    ChargerSprite("../Image/carte8.png"),
+    ChargerSprite("../Image/carte9.png"),
+    ChargerSprite("../Image/carte10.png"),
+    ChargerSprite("../Image/carte11.png"),
+    ChargerSprite("../Image/carte12.png"),
+    ChargerSprite("../Image/carte13.png"),
+    ChargerSprite("../Image/carte14.png"),
+    ChargerSprite("../Image/carte15.png"),
+    ChargerSprite("../Image/carte16.png"),
+    ChargerSprite("../Image/carte17.png"),
+    ChargerSprite("../Image/carte18.png"),
+    ChargerSprite("../Image/carte19.png"),
+    ChargerSprite("../Image/carte20.png")
+  };
 
-    int backcard = ChargerSprite("../Image/back.png");
-    int spritesRand[40]; /*tableau destiné a accueillir les valeurs random de sprite*/
-    
-    for (i = 0; i < spriterandsize; i ++)
-    {     /*incremente le tableau random de 0*/
-      
-      spritesRand[i] = 0;
-    }
+  int backcard = ChargerSprite("../Image/back.png");
+  int spritesRand[40]; /*tableau destiné a accueillir les valeurs random de sprite*/
+  
+  for (i = 0; i < spriterandsize; i ++)
+  {     /*incremente le tableau random de 0*/
+    spritesRand[i] = 0;
+  }
 
-    for (i = 0; i < spritesize; i++)
-    {  /*boucle dans le tab des sprites pour garder le sprite courant*/
-      int current = sprite[i];
-      for (j = 0; j < 2; ) {  /*sort de la boucle si le currentsprite a été placé 2fois dans le tab rand*/
-        int ran = (rand() % (MAX - MIN) + MIN);
-        if (spritesRand[ran] == 0)
-        {  /*le curseur cherche aléatoirement un espace dans le tab*/
-          spritesRand[ran] = current;
-          j++;
-        }
+  for (i = 0; i < spritesize; i++)
+  {  /*boucle dans le tab des sprites pour garder le sprite courant*/
+    int current = sprite[i];
+    for (j = 0; j < 2; )
+    {  /*sort de la boucle si le currentsprite a été placé 2fois dans le tab rand*/
+      int ran = (rand() % (MAX - MIN) + MIN);
+      if (spritesRand[ran] == 0)
+      {  /*le curseur cherche aléatoirement un espace dans le tab*/
+        spritesRand[ran] = current;
+        j++;
       }
     }
+  }
 
-    int grillerand[5][8];
-    int ligne, colonne;
-    i = 0;
+  int grillerand[5][8]; /*initialisation du tableau contenant les cartes aléatoires*/
+  int ligne, colonne;
+  i = 0;
 
-    for ( ligne = 0; ligne < cartex; ligne++ )
+  for ( ligne = 0; ligne < cartex; ligne++ )
+  {
+    for (colonne = 0; colonne < cartey; colonne++)
     {
-      for (colonne = 0; colonne < cartey; colonne++)
-      {
-        grillerand[ligne][colonne] = spritesRand[i];
-        AfficherSprite(backcard, x, y);
-        x = x + marge + longueurImage;
-        i++;
-      }
+      grillerand[ligne][colonne] = spritesRand[i]; /*On attribue les sprite aux différentes cases du tableau*/
+      AfficherSprite(backcard, x, y); /*On affiche les dos de cartes dans toutes les cases du tableau*/
+      x = x + marge + longueurImage;
+      i++;
+    }
+    x = marge;
+    y = y + marge + largeurImage;
+  }
+
+  int selection = 0, score = 0, selectionx = 0, selectiony = 0;
+  int tempo = 0;
+  ChoisirEcran(2); /*On utilise un second écran virtuel avec un fond dans lequel on fera tourner le compteur*/
+  ChargerImageFond("../Image/fond.png");
+  ChoisirEcran(0);
+
+  int spritetrouve[20], k, dejatrouve = 0; /*tableau destiné a contenir les sprites dejà trouvé*/
+  
+  for (k = 0; k < 20; k++)
+  {
+    spritetrouve[k] = 0;
+  }
+
+  while (goon == 1)
+  {
+    if (ToucheEnAttente() && Touche() == XK_t && tempo == 0) /*initialisation du mode triche*/
+    {
+      int ligne, colonne;
+      tempo = 1;
+      CopierZone(0,3,0,70,(longueurImage*cartey + marge*cartey),(largeurImage*cartex + marge*cartex),0,70); /*copie de l'écran actuelle dans un écran tampon*/
+      ChoisirEcran(2);
       x = marge;
-      y = y + marge + largeurImage;
-    }
+      y = 70;
 
-    int selection = 0, score = 0, selectionx = 0, selectiony = 0;
-    int tempo = 0;
-        ChoisirEcran(2);
-        ChargerImageFond("../Image/fond.png");
-        ChoisirEcran(0);
-
-    int spritetrouve[20], k, dejatrouve = 0;
-    
-    for (k = 0; k < 20; k++){
-      spritetrouve[k] = 0;
-    }
-
-    while (goon == 1)
-    {
-      if (ToucheEnAttente() && Touche() == XK_t && tempo == 0)
+      for ( ligne = 0; ligne < cartex; ligne++ ) /*boucle qui affiche les cartes côté sprite dans la grille*/
       {
-        int ligne, colonne;
-        tempo = 1;
-        CopierZone(0,3,0,70,(longueurImage*cartey + marge*cartey),(largeurImage*cartex + marge*cartex),0,70);
-        ChoisirEcran(2);
-        x = marge;
-        y = 70;
-          for ( ligne = 0; ligne < cartex; ligne++ )
-          {
-            for (colonne = 0; colonne < cartey; colonne++)
-            {
-              AfficherSprite(grillerand[ligne][colonne], x, y);
-              x = x + marge + longueurImage;
-            }
-            x = marge;
-            y = y + marge + largeurImage;
-          }
-          CopierZone(2,0,0,70,(longueurImage*cartey + marge*cartey),(largeurImage*cartex + marge*cartex),0,70);
-
-        while (tempo == 1)
+        for (colonne = 0; colonne < cartey; colonne++)
         {
-          if (ToucheEnAttente() && Touche() == XK_t)
-          {
-            tempo = 0;
-          }
+          AfficherSprite(grillerand[ligne][colonne], x, y);
+          x = x + marge + longueurImage;
         }
-        CopierZone(3,0,0,70,(longueurImage*cartey + marge*cartey),(largeurImage*cartex + marge*cartex),0,70);
+        x = marge;
+        y = y + marge + largeurImage;
       }
+      CopierZone(2,0,0,70,(longueurImage*cartey + marge*cartey),(largeurImage*cartex + marge*cartex),0,70);
 
-    if (Microsecondes()>temps)
+      while (tempo == 1)
+      {
+        if (ToucheEnAttente() && Touche() == XK_t) /*Condition de sortie du mode triche*/
+        {
+          tempo = 0;
+        }
+      }
+      CopierZone(3,0,0,70,(longueurImage*cartey + marge*cartey),(largeurImage*cartex + marge*cartex),0,70);/*On reprend l'écran précedemment sauvegardé dans l'écran tampon*/
+    }
+
+    if (Microsecondes()>temps) /*timer*/
     {
       seconde++;
       ChoisirEcran(1);
@@ -151,39 +153,49 @@ void Grille(int cartex, int cartey)
     x = marge;
     y = 70;
 
-    if(SourisCliquee())
+    if(SourisCliquee()) /*condition qui gère le clique de la souris pour retourner les cartes*/
     {
-      for(ligne = 0; ligne < cartex; ligne++ )
+      for(ligne = 0; ligne < cartex; ligne++)
       {
         for(colonne = 0; colonne < cartey; colonne++)
         {
           if ((_X >= x) && (_X <= x + longueurImage) && (_Y >= y) && (_Y <= y + largeurImage))
           {
-            for(k = 0; k < 20; k++){
-              if (spritetrouve[k] == spritesRand[i]) {
+            for(k = 0; k < 20; k++)
+            {
+              if (spritetrouve[k] == spritesRand[i]) /*vérification si la paire de carte est identique*/
+              {
                 dejatrouve = 1;
               }
             }
+
             AfficherSprite(spritesRand[i],x, y);
             sleep(1); /*mise en veille du programme de 1sec pour la seconde selection, permet de le voir*/
-            if(selection == 0 && dejatrouve == 0){ /*stocke la valeur du clique dans selection, ses coordonnées dans selectionx et selectiony*/
+            if(selection == 0 && dejatrouve == 0)
+            { /*stocke la valeur du clique dans selection, ses coordonnées dans selectionx et selectiony*/
               selection = spritesRand[i];
               selectionx = x;
               selectiony = y;
             }
-            else {
-              if(selection == spritesRand[i] && (selectionx != x || selectiony != y) ){ /*si le second clique rempli ses conditions alors on a trouvé une paire*/
-                  score++;  /*le score s'incrémente jusqu'a avoir trouvé les 10paires (ici) donc si score == 10, goon = 0 (voir plus bas) */
-                  for ( k = 0; k < 20; k++){
-                    if (spritetrouve[k] == 0){
-                      spritetrouve[k] = selection;
-                      break;
-                    }
+            else
+            {
+              if(selection == spritesRand[i] && (selectionx != x || selectiony != y) )
+              { /*si le second clique rempli ses conditions alors on a trouvé une paire*/
+                score++;  /*le score s'incrémente jusqu'a avoir trouvé les 10paires (ici) donc si score == 10, goon = 0 (voir plus bas) */
+                for ( k = 0; k < 20; k++)
+                {
+                  if (spritetrouve[k] == 0)
+                  {
+                    spritetrouve[k] = selection; /*si la carte a déjà été trouvée on ne réaffiche pas le dos par dessus*/
+                    break;
                   }
+                }
               }
-              else {
-                if (dejatrouve == 0){
-                AfficherSprite(backcard, x, y);  /*si faux, réaffiche le bac par dessus la premi!re selection et la deuxeieme*/
+              else
+              {
+                if (dejatrouve == 0)
+                {
+                  AfficherSprite(backcard, x, y);  /*si faux, réaffiche le bac par dessus la premi!re selection et la deuxeieme*/
                 }
                 AfficherSprite(backcard, selectionx, selectiony);
               }
@@ -198,11 +210,13 @@ void Grille(int cartex, int cartey)
         y = y + marge + largeurImage;
       }
     } 
-    if(score == spritesize){ /* condition de victoire, return 0 pour montrer la fin*/
+    if(score == spritesize)
+    { /* condition de victoire, return 0 pour montrer la fin*/
       goon = 0;  
     }
-if (ToucheEnAttente() && Touche() == XK_q){
-    goon = 0;
+    if (ToucheEnAttente() && Touche() == XK_q) /*on peut appuyer sur q pour quitter*/
+    {
+      goon = 0;
+    }
   }
-}
 }
--- Programme/main.c
+++ Programme/main.c
@@ -22,80 +22,87 @@ void Regle(void); /*affiche une fenetre avec les règles du jeu*/
 void Grille(int cartex, int cartey); /*initialise la grille de jeu en fonction
  de la difficulté avec des tailles différentes*/
 
-int main(void){ 
-    InitialiserGraphique();   /*créer la fenetre */
-    CreerFenetre(10,25,1400,720);
-    Menu();
-    Touche();
-    FermerGraphique();
+int main(void)
+{ 
+  InitialiserGraphique();   /*créer la fenetre */
+  CreerFenetre(10,25,1400,720);
+  Menu();
+  Touche();
+  FermerGraphique();
 }
 
-void Menu(){
-    ChargerImageFond("../Image/projet.png"); 
-    /*permet de connaitre la position actuelle du curseur */
-    SourisPosition();
-    while (goon == 0)
-    {
-        SourisCliquee();
-        if ( (_X >= 770) && (_X <= 833) && (_Y >= 350) && (_Y <= 413) ) /*si l'utilisateur clique dans cette surface 
-        (qui correspond a un bouton) alors on ferme la fenetre actuelle pour en ouvrir une autre contenant les différentes grilles de jeu*/
-        {
-            Facile();
-        }
-        else if ( _X >= 620 && _X <= 683 && _Y >= 445 && _Y <= 508 )
-        {
-            Normal();
-        }
-        else if ( _X >= 810 && _X <= 873 && _Y >= 555 && _Y <= 618 )
-        {
-            Difficile();
-        }
-        else if ( _X >= 159 && _X <= 404 && _Y >= 294 && _Y <= 586 )
-        {
-            Regle();
-            SourisCliquee();
-        }
-    }
-}
-
-void Regle(void){
-    while (!SourisCliquee())
-    {
-      ChargerImageFond("../Image/regle22.png");
+void Menu()
+{
+  ChargerImageFond("../Image/projet.png"); 
+  /*permet de connaitre la position actuelle du curseur */
+  SourisPosition();
+  while (goon == 0)
+  {
       SourisCliquee();
-      if ( (_X >= 1025) && (_X <= 1095) && (_Y >= 645) && (_Y <= 715) )
+      if ( (_X >= 770) && (_X <= 833) && (_Y >= 350) && (_Y <= 413) ) /*si l'utilisateur clique dans cette surface 
+      (qui correspond a un bouton) alors on ferme la fenetre actuelle pour en ouvrir une autre contenant les différentes grilles de jeu*/
       {
-        Menu();
+          Facile();
       }
+      else if ( _X >= 620 && _X <= 683 && _Y >= 445 && _Y <= 508 )
+      {
+          Normal();
+      }
+      else if ( _X >= 810 && _X <= 873 && _Y >= 555 && _Y <= 618 )
+      {
+          Difficile();
+      }
+      else if ( _X >= 159 && _X <= 404 && _Y >= 294 && _Y <= 586 )
+      {
+          Regle();
+          SourisCliquee();
+      }
+  }
+}
+
+void Regle(void)
+{
+  while (!SourisCliquee())
+  {
+    ChargerImageFond("../Image/regle22.png");
+    SourisCliquee();
+    if ( (_X >= 1025) && (_X <= 1095) && (_Y >= 645) && (_Y <= 715) )
+    {
+      Menu();
     }
   }
-
-void Facile(){
-    /*grille facile*/
-    Grille(4,5);
-    Victoire();
 }
 
-void Normal(){
-    /*grille normal*/
-    Grille(5,6);
-    Victoire();
+void Facile()
+{
+  /*grille facile*/
+  Grille(4,5);
+  Victoire();
 }
 
-void Difficile(){
-    /*grille difficile*/
-    Grille(5,8);
-    Victoire();
+void Normal()
+{
+  /*grille normal*/
+  Grille(5,6);
+  Victoire();
 }
 
-void Victoire(void){
-    while (!SourisCliquee())
+void Difficile()
+{
+  /*grille difficile*/
+  Grille(5,8);
+  Victoire();
+}
+
+void Victoire(void)
+{
+  while (!SourisCliquee())
+  {
+    ChargerImageFond("../Image/victoire.png");
+    SourisCliquee();
+    if ( (_X >= 1010) && (_X <= 1210) && (_Y >= 545) && (_Y <= 645) )
     {
-      ChargerImageFond("../Image/victoire.png");
-      SourisCliquee();
-      if ( (_X >= 1010) && (_X <= 1210) && (_Y >= 545) && (_Y <= 645) )
-      {
-        Menu();
-      }
+      Menu();
+    }
   }
 }
